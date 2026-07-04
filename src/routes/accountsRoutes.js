bcb4332046ec67b7d804bc180704b0cc477a4c7d
haile <desu3096@gmail.com>
2026-07-04 06:36:29 +0300

    commit for fix account routing

--- src/routes/accountsRoutes.js
+++ src/routes/accountsRoutes.js
@@ -1,34 +1,38 @@
 import AccountsDashboard from "../pages/accounts/AccountsDashboard.js";
+import UserManagementCard from "../pages/accounts/UserManagementCard.jsx";
+import RoleManagementCard from "../pages/accounts/RoleManagementCard.jsx";
+import ChangePasswordCard from "../pages/accounts/ChangePasswordCard.jsx";
+import ResetPasswordCard from "../pages/accounts/ResetPasswordCard.jsx";
+import AccountListCard from "../pages/accounts/AccountListCard.jsx";
+
+// Keep old imports commented out if needed later for reference
 /*
 import PageAccessManagement from "../pages/accounts/PageRoleMapping.js";
 import AdvancedAccessManagement from "../pages/accounts/AdvancedAccessManager.js";
-
 import Roles from "../pages/accounts/Roles.js";
 import RoleApiMap from "../pages/accounts/RoleApiMap.js";
-import AccountList from "../pages/accounts/AccountList.js";
-
-import ChangePassword from "../pages/accounts/ChangePassword.js";
 import ChangeProfile from "../pages/accounts/ChangeProfile.js";
-
 import AssignRolesToAccount from "../pages/accounts/AssignRolesToAccount.js";
 import ListOfRolesPrevillages from "../pages/accounts/ListOfEndPoints.js";
 import StaffUserProfile from "../pages/accounts/StaffUserProfile.js";
-import UserManagementPage from "../pages/accounts/UserManagementPage.js";
 */
+
 export const accountsRoutes = [
   { path: "accounts-dashboard", element: <AccountsDashboard /> },
+  { path: "accounts-dashboard/user-management", element: <UserManagementCard /> },
+  { path: "accounts-dashboard/role-management", element: <RoleManagementCard /> },
+  { path: "accounts-dashboard/change-password", element: <ChangePasswordCard /> },
+  { path: "accounts-dashboard/reset-password", element: <ResetPasswordCard /> },
+  { path: "accounts-dashboard/account-list", element: <AccountListCard /> },
+  
   /*
   { path: "change-pprofile", element: <ChangeProfile /> },
   { path: "roles", element: <Roles /> },
   { path: "role-api-maps", element: <RoleApiMap /> },
-  { path: "account-list", element: <AccountList /> },
   { path: "page-access-management", element: <PageAccessManagement /> },
   { path: "advanced-access-management", element: <AdvancedAccessManagement /> },
-
-  { path: "user-management", element: <UserManagementPage /> },
-  { path: "change-password", element: <ChangePassword /> },
   { path: "assign-role-and-account", element: <AssignRolesToAccount /> },
   { path: "list-of-roles-previllages", element: <ListOfRolesPrevillages /> },
   { path: "profile", element: <StaffUserProfile /> },
-   */
+  */
 ];
